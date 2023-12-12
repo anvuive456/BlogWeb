77013d0ba81b0fc3bfaf93031839b2571501e940
antran <trannhatan2803@gmail.com>
2023-12-12 15:13:08 +0700

    - update landscape article card

--- src/components/LandScapeArticleCard.tsx
+++ src/components/LandScapeArticleCard.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const LandScapeArticleCard = ({post} : {post: Post})=>{
   return  (
-      <div className='flex flex-row gap-2 my-2'>
+      <div className='flex flex-row gap-5 my-2'>
         <Image src={post.image} alt={post.title} width={250} height={250}/>
         <div className='flex flex-col gap-2 justify-center'>
           <Link href={post.url} className='text-xl font-semibold'>{post.title}</Link>
